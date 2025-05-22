11a0a22480d6c47b97ce7b2bad86f921d162c891
Brayan Mendoza <95663591+BrayanMen@users.noreply.github.com>
2025-05-22 00:04:00 +0000

    hero section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import './HomePage.css';
 import { useProduct } from '../context/ProductContext';
 
 export default function HomePage() {
     const {products, loading} = useProduct()
     const [error, setError] = useState(null);
-
-    const url = 'https://6825eaad397e48c913143248.mockapi.io/products';
-
-    
-
+  
     if (loading) return <div className="">Cargando productos...</div>;
     if (error) return <div className="">{error}</div>;
 
     return (
         <div>
+        <section id="hero_section" className="hero_section">
+        <div className="hero_mask"></div>
+        <div 
+        className="hero_background"          
+        >
+        </div>
+            <div className="hero__content">
+                <h1 className="hero__title">Bienvenido al FUTURO</h1>
+                <h3 className="hero__description">Explora tu estilo TeckWear.</h3>
+                <a href="#products" className="btn btn--primary">Ver productos</a>
+            </div>
+        </section>
             <div>
                 <div>
                     {products.map(d => {
